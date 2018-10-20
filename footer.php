0e418e2d831eb409ae9c7cc8dcb79da3df2ff95c
EgleGvi <e.gvildyte@gmail.com>
2018-10-20 16:01:53 +0300

    iki footer

--- footer.php
+++ footer.php
@@ -1,6 +1,41 @@
 <footer>
 
-
+<div class="footer-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h5 class="name">Lorem</h5>
+            </div>
+            <div class="col-md-6">
+                <h5 class="name">Lorem ipsum</h5>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-1">
+                <p class="nov">17 NOV</p>
+            </div>
+            <div class="col-md-6">
+                <div class="wrapper-green">
+                    <h5 class="name2">Inani referrentur mei et. </h5>
+                    <p class="text">Eu per natum qualisque accommodare, ea discere disputando.</p>
+                    <a class="more" href="#">Daugiau...</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-1">
+                <p class="nov">17 NOV</p>
+            </div>
+            <div class="col-md-6">
+                <div class="wrapper-green">
+                    <h5 class="name2">Inani referrentur mei et. </h5>
+                    <p class="text">Eu per natum qualisque accommodare, ea discere disputando.</p>
+                    <a class="more" href="#">Daugiau...</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
