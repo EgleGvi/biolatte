4c2e5e1a43ae36622ccd9b0eaaa084a28f6da269
EgleGvi <e.gvildyte@gmail.com>
2018-10-22 22:37:10 +0300

    biolatte header

--- footer.php
+++ footer.php
@@ -3,39 +3,53 @@
 <div class="footer-wrapper">
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-2"></div>
+            <div class="col-md-5">
                 <h5 class="name">Lorem</h5>
+                <div class="wrapper-green">
+                    <p class="nov">17 NOV</p>
+                    <p class="name2">Inani referrentur mei et. </p>
+                    <p class="text">Eu per natum qualisque accommodare, ea discere disputando.</p>
+                    <a class="more" href="#">Daugiau...</a>
+                </div>
+                <div class="wrapper-green">
+                    <p class="nov">17 NOV</p>
+                    <p class="name2">Inani referrentur mei et.</p>
+                    <p class="text">Eu per natum qualisque accommodare, ea discere disputando.</p>
+                    <a class="more" href="#">Daugiau...</a>
+                </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <h5 class="name">Lorem ipsum</h5>
+                <i class="fas fa-home"></i>
+                <span class="house">Ansoleil OU<br>Reg.nr:123456<br>KMKR nr:123456789<br>Angeelika Vollmann<br></span>
+                <br>
+                <i class="fas fa-phone"></i>
+                <span class="house">+370645998722<br></span>
+                <br>
+                <i class="fas fa-envelope"></i>
+                <span class="house mail">info@biolatte.ee</span>
             </div>
+            <div class="col-md-1"></div>
         </div>
+    </div>
+</div>
+<div class="footer2">
+    <div class="container">
         <div class="row">
-            <div class="col-md-1">
-                <p class="nov">17 NOV</p>
+            <div class="col-md-4">
+                <p class="biolatte">Biolatte &#169 2014</p>
             </div>
-            <div class="col-md-6">
-                <div class="wrapper-green">
-                    <h5 class="name2">Inani referrentur mei et. </h5>
-                    <p class="text">Eu per natum qualisque accommodare, ea discere disputando.</p>
-                    <a class="more" href="#">Daugiau...</a>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-1">
-                <p class="nov">17 NOV</p>
-            </div>
-            <div class="col-md-6">
-                <div class="wrapper-green">
-                    <h5 class="name2">Inani referrentur mei et. </h5>
-                    <p class="text">Eu per natum qualisque accommodare, ea discere disputando.</p>
-                    <a class="more" href="#">Daugiau...</a>
-                </div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <p class="kontakt">Avaleht</p>
+                <p class="kontakt">KKK</p>
+                <p class="kontakt">Kontakt</p>
             </div>
         </div>
     </div>
 </div>
+
 
 
 
--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -11,28 +12,43 @@
 </head>
 <body>
 <header>
-    <div class="background-cover">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="shadow1">
-                        <ul>
-                            <li><a href="#">Login</a></li>
-                            <li><a href="#">Register</a></li>
-                            <li><a href="#">Facebook</a></li>
-                            <li><a href="#">Cart</a></li>
-                        </ul>
-                    </div>
-                    <div class="slider2">
-                        <div class="logo1">
-                            <img src="img/logo.png">
+    <div class="header-block">
+        <div class="header-top">
+            <div class="main-top-background"></div>
+            <div class="main-top-shadow"></div>
+            <div class="main-top-content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="content-padding-right">
+                                <a href="#" class="cart-shop">Cart</a>
+                                <ul class="subheader-menu">
+                                    <li><a href="#">Login</a></li>
+                                    <li><a href="#">Register</a></li>
+                                    <li><a href="#">Facebook</a></li>
+                                </ul>
+                            </div>
                         </div>
-                        <div class="list1">
+                    </div>
+                </div>
+            </div>
+            <div class="main-top-stripes"></div>
+        </div>
+        <div class="slider-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <a href="#" class="logo-container">
+                            <img class="img-responsive" src="img/logo.png" alt="" title="">
+                        </a>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="header-menu">
                             <ul>
                                 <li><a href="#">Home</a></li>
                                 <li><a href="#">Blog</a></li>
                                 <li><a href="#">Events</a></li>
-                                <li><a href="#">Store locator</a></li>
+                                <li><a href="#">Store Locator</a></li>
                                 <li><a href="#">Contact us</a></li>
                                 <li><a href="#">How to Purchase</a></li>
                             </ul>
@@ -41,6 +57,9 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="slider-block">
+        <img class="img-responsive" src="img/slider1.png" alt="" title="">
     </div>
 
 </header>
